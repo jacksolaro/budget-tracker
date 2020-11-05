b888851f857f48924c80b6418d80bf6e98ffdf9f
jacksolaro <jacksolaro@gmail.com>
2020-11-04 20:03:30 -0800

    moved service worker back to index.html

--- public/index.js
+++ public/index.js
@@ -152,12 +152,4 @@ document.querySelector("#sub-btn").onclick = function() {
   sendTransaction(false);
 };
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-  .then(function(registration) {
-    console.log('Registration successful, scope is:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service worker registration failed, error:', error);
-  });
-}
+
